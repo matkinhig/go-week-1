e6b527df49da7f98c74a64193117558b79752149
PLeo <phongleo92@gmail.com>
2020-02-29 01:00:38 +0700

    sync code

--- luc-dien-phong/homework/homeworkday1.go
+++ luc-dien-phong/homework/homeworkday1.go
@@ -2,11 +2,6 @@ package homework
 
 import "fmt"
 
-//PrintName comment
-func PrintName() {
-	fmt.Println("Test")
-}
-
 // Max cmt
 func Max(arr []int) (resul int, err string) {
 	if len(arr) == 0 {
@@ -52,4 +47,54 @@ func Contains(arr []int, value int) (result int, err string) {
 	}
 
 	return result, fmt.Sprintf("%d not found", value)
+}
+
+// Difference cmt
+func Difference(arr []int, others []int) (result []int, err string) {
+	if len(arr) == 0 {
+		return result, "arr not empty"
+	}
+	if len(others) == 0 {
+		return result, "others not empty"
+	}
+
+	for i := 0; i < len(arr); i++ {
+		found := false
+		for e := 0; e < len(others); e++ {
+			if arr[i] == others[e] {
+				found = true
+				break
+			}
+		}
+		if !found {
+			result = append(result, arr[i])
+		}
+	}
+
+	if len(result) == 0 {
+		return result, "no other value"
+	}
+
+	return result, ""
+}
+
+// IndexOf , return index
+func IndexOf(arr []int, value int) (result int, err string) {
+	if len(arr) == 0 {
+		return result, "arr not empty"
+	}
+
+	result = -1
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == value {
+			result = i
+			break
+		}
+	}
+
+	if result == -1 {
+		return result, "not found"
+	}
+	return result, ""
+
 }
--- luc-dien-phong/homework/homeworkday1_test.go
+++ luc-dien-phong/homework/homeworkday1_test.go
@@ -6,7 +6,7 @@ import "testing"
 func TestMax1(t *testing.T) {
 	result, err := Max([]int{0, 1, 2, 3})
 	if len(err) > 0 {
-		t.Error("TestMax1 error", err)
+		t.Error("TestMax1 error: ", err)
 	} else {
 		t.Log("TestMax1 success. result: ", result)
 	}
@@ -14,7 +14,7 @@ func TestMax1(t *testing.T) {
 func TestMax2(t *testing.T) {
 	result, err := Max([]int{})
 	if len(err) > 0 {
-		t.Error("TestMax2 error", err)
+		t.Error("TestMax2 error: ", err)
 	} else {
 		t.Log("TestMax2 success. result: ", result)
 	}
@@ -24,7 +24,7 @@ func TestMax2(t *testing.T) {
 func TestMin1(t *testing.T) {
 	result, err := Min([]int{0, 1, 2, 3})
 	if len(err) > 0 {
-		t.Error("TestMin1 error", err)
+		t.Error("TestMin1 error: ", err)
 	} else {
 		t.Log("TestMin1 success. result: ", result)
 	}
@@ -32,7 +32,7 @@ func TestMin1(t *testing.T) {
 func TestMin2(t *testing.T) {
 	result, err := Min([]int{})
 	if len(err) > 0 {
-		t.Error("TestMin2 error", err)
+		t.Error("TestMin2 error: ", err)
 	} else {
 		t.Log("TestMin2 success. result: ", result)
 	}
@@ -40,7 +40,7 @@ func TestMin2(t *testing.T) {
 func TestMin3(t *testing.T) {
 	result, err := Min([]int{0, -1, -2, 3, 4, 5})
 	if len(err) > 0 {
-		t.Error("TestMin3 error", err)
+		t.Error("TestMin3 error: ", err)
 	} else {
 		t.Log("TestMin3 success. result: ", result)
 	}
@@ -49,7 +49,7 @@ func TestMin3(t *testing.T) {
 func TestContain1(t *testing.T) {
 	result, err := Contains([]int{0, -1, -2, 3, 4, 5}, -2)
 	if len(err) > 0 {
-		t.Error("TestContain1 error", err)
+		t.Error("TestContain1 error: ", err)
 	} else {
 		t.Log("TestContain1 success. result: ", result)
 	}
@@ -57,16 +57,84 @@ func TestContain1(t *testing.T) {
 func TestContain2(t *testing.T) {
 	result, err := Contains([]int{}, -2)
 	if len(err) > 0 {
-		t.Error("TestContain2 error", err)
+		t.Error("TestContain2 error: ", err)
 	} else {
 		t.Log("TestContain2 success. result: ", result)
 	}
 }
-func TestContain23(t *testing.T) {
+func TestContain3(t *testing.T) {
 	result, err := Contains([]int{0, -1, -2, 3, 4, 5}, 100)
 	if len(err) > 0 {
-		t.Error("TestContain23 error", err)
+		t.Error("TestContain3 error: ", err)
 	} else {
-		t.Log("TestContain23 success. result: ", result)
+		t.Log("TestContain3 success. result: ", result)
+	}
+}
+
+// Difference testing
+func TestDifference1(t *testing.T) {
+	result, err := Difference([]int{0, -1, -2, 3, 4, 5}, []int{0, -2, 4})
+	if len(err) > 0 {
+		t.Error("TestDifference1 error: ", err)
+	} else {
+		t.Log("TestContTestDifference1ain23 success. result: ", result)
+	}
+}
+func TestDifference2(t *testing.T) {
+	result, err := Difference([]int{0, -1, -2, 3, 4, 5}, []int{6, 7, 9})
+	if len(err) > 0 {
+		t.Error("TestDifference2 error: ", err)
+	} else {
+		t.Log("TestDifference2 success. result: ", result)
+	}
+}
+func TestDifference3(t *testing.T) {
+	result, err := Difference([]int{0, -1, -2, 3, 4, 5}, []int{0, -1, -2, 3, 4, 5})
+	if len(err) > 0 {
+		t.Error("TestDifference3 error: ", err)
+	} else {
+		t.Log("TestDifference3 success. result: ", result)
+	}
+}
+func TestDifference4(t *testing.T) {
+	result, err := Difference([]int{1, 2, 3}, []int{})
+	if len(err) > 0 {
+		t.Error("TestDifference4 error: ", err)
+	} else {
+		t.Log("TestDifference4 success. result: ", result)
+	}
+}
+func TestDifference5(t *testing.T) {
+	result, err := Difference([]int{}, []int{1, 2, 3})
+	if len(err) > 0 {
+		t.Error("TestDifference5 error: ", err)
+	} else {
+		t.Log("TestDifference5 success. result: ", result)
+	}
+}
+
+// IndexOf testing
+func TestIndexOf1(t *testing.T) {
+	result, err := IndexOf([]int{1, 2, 3}, 1)
+	if len(err) > 0 {
+		t.Error("TestIndexOf1 error: ", err)
+	} else {
+		t.Log("TestIndexOf1 success. result: ", result)
+	}
+}
+func TestIndexOf2(t *testing.T) {
+	result, err := IndexOf([]int{1, 2, 3}, 0)
+	if len(err) > 0 {
+		t.Error("TestIndexOf2 error: ", err)
+	} else {
+		t.Log("TestIndexOf2 success. result: ", result)
+	}
+}
+func TestIndexOf3(t *testing.T) {
+	result, err := IndexOf([]int{}, 0)
+	if len(err) > 0 {
+		t.Error("TestIndexOf3 error: ", err)
+	} else {
+		t.Log("TestIndexOf3 success. result: ", result)
 	}
 }
