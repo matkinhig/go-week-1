1d865b63b4fa357e4fa3cc7319e7c5ad3dd076c7
Tiến Lực Nguyễn <matkinhig@gmail.com>
2020-02-28 23:50:43 +0700

    config gomod

---
+++ week-1/1/handle/handle.go
@@ -0,0 +1,17 @@
+package handle
+
+import "fmt"
+
+type Tester interface {
+	Test()
+}
+
+type MyFloat float64
+
+func (m MyFloat) Test() {
+	fmt.Println(m)
+}
+
+func describe(t Tester) {
+	fmt.Printf("Interface type %T value %v\n", t, t)
+}
--- week-1/1/main.go
+++ week-1/1/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import ( 
+	"fmt"
+	"github.com/matkinhig/golang-course/week-1/1/handle"
+)
 
 func main() {
 	var t handle.Tester()
+	t = f
+	fmt.Printf("Interface type %T value %v\n", t, t)
+	t.Test()
 }
