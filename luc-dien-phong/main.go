906041c680ae6d22733186b31d2655feb5b92696
PLeo <phongleo92@gmail.com>
2020-03-06 22:10:19 +0700

    Sync code

--- luc-dien-phong/main.go
+++ luc-dien-phong/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+// Point cmt
+type Point struct {
+	X int
+	y int
+}
+
+// FuncPointer cmt
+func FuncPointer() {
+	p1 := Point{9, 10}
+	fmt.Println(p1)
+	p2 := &p1
+	p2.X = -9
+	fmt.Println(p1)
+}
+
 func main() {
 	fmt.Println("Pls run file: homeworkday1_test.go")
+	FuncPointer()
 }
